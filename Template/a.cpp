24c3e629429471eed8d0064c5df2dd6ee9f4039e
Zai Santillan <57343545+plskz@users.noreply.github.com>
2022-11-14 13:09:05 +0800

    chore: format

--- Template/a.cpp
+++ Template/a.cpp
@@ -7,8 +7,8 @@ using ll = long long;
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    return 0;
+  return 0;
 }
--- Template/db.cpp
+++ Template/db.cpp
@@ -22,37 +22,37 @@ void __print(bool x) { cerr << (x ? "true" : "false"); }
 
 template <typename T, typename V>
 void __print(const pair<T, V> &x) {
-    cerr << '{';
-    __print(x.first);
-    cerr << ',';
-    __print(x.second);
-    cerr << '}';
+  cerr << '{';
+  __print(x.first);
+  cerr << ',';
+  __print(x.second);
+  cerr << '}';
 }
 template <typename T>
 void __print(const T &x) {
-    int f = 0;
-    cerr << '{';
-    for (auto &i : x) cerr << (f++ ? "," : ""), __print(i);
-    cerr << "}";
+  int f = 0;
+  cerr << '{';
+  for (auto &i : x) cerr << (f++ ? "," : ""), __print(i);
+  cerr << "}";
 }
 void _print() { cerr << "]\n"; }
 template <typename T, typename... V>
 void _print(T t, V... v) {
-    __print(t);
-    if (sizeof...(v)) cerr << ", ";
-    _print(v...);
+  __print(t);
+  if (sizeof...(v)) cerr << ", ";
+  _print(v...);
 }
 #ifndef ONLINE_JUDGE
-#define debug(x...)               \
-    cerr << "[" << #x << "] = ["; \
-    _print(x)
+#define debug(x...)             \
+  cerr << "[" << #x << "] = ["; \
+  _print(x)
 #else
 #define debug(x...)
 #endif
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    return 0;
+  return 0;
 }
